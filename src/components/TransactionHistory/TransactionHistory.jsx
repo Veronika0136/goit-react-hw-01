814bf279ec018ecf07f2c92031d3b3d0d4928b6e
veronika <artemova331@gmail.com>
2025-03-27 00:34:43 +0200

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,31 +1,26 @@
 import React from 'react';
-// import s from '../TransactionHistory/TransactionHistory.module.css';
+import s from '../TransactionHistory/TransactionHistory.module.css';
 
-const TransactionHistory = () => {
+const TransactionHistory = ({ items }) => {
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Type</th>
-            <th>Amount</th>
-            <th>Currency</th>
+    <table className={s.transaction_table}>
+      <thead className={s.transaction_thead}>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr className={s.transaction_tr} key={id}>
+            <td className={s.transaction_td}>{type}</td>
+            <td className={s.transaction_td}>{amount}</td>
+            <td className={s.transaction_td}>{currency}</td>
           </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Invoice</td>
-            <td>125</td>
-            <td>USD</td>
-          </tr>
-          <tr>
-            <td>Withdrawal</td>
-            <td>85</td>
-            <td>USD</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
